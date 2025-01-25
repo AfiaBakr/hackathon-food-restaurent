42a0699e03f2788cfbcca43a632f0387aff48151
AfiaBakr <159829947+AfiaBakr@users.noreply.github.com>
2025-01-26 03:09:55 +0500

    add contact route page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Footer from "../Components/Footer";
+
+export default function Contact() {
+    return (
+      <div>
+     
+      <Footer/>
+      </div>
+    );
+  }
+  
